4274414ee4c8c9b9530b38defb7578ee336454e9
Baska <baska.klimekova@gmail.com>
2021-10-22 10:31:24 +0200

    updating css style

--- Ukol01-filmy-main/src/components/Movie/index.jsx
+++ Ukol01-filmy-main/src/components/Movie/index.jsx
@@ -7,18 +7,19 @@ const Movie = (props) => {
 return (
     <div className="movie">
         <div className="moviePoster">
-            <img src={'../assets/'+ props.movie.poster} />
+            <img className="poster" src={'../assets/'+ props.movie.poster} />
         </div>
         <div className="movieRating"> 
             <p>{props.movie.rating}/10</p>
         </div>
         <div className="description">
             <h2> {props.movie.title} </h2>
-            <p className="year">Rok vydani: {props.movie.year}</p>
-            <p className="genre">Zaner:  {props.movie.genre}</p>
-            <p className="director">Rezie: {props.movie.director} </p>
+            <p className="year"> <b>Rok vydani: </b> {props.movie.year}</p>
+            <p className="genre"><b>Zaner: </b>  {props.movie.genre}</p>
+            <p className="director"><b>Rezie: </b> {props.movie.director} </p>
 
             <div className="role"> 
+            <br/>
                 <h3>V hlavnich rolich: </h3>
                 {
                 props.movie.cast.map(role =>{ return<Role actor={role.name} role={role.as} key={role.name}/>})
