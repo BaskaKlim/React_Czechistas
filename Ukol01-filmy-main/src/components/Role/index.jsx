7a3524223905d5976447e3871d3ff5d4310a6bed
Baska <baska.klimekova@gmail.com>
2021-10-22 09:43:59 +0200

    updating css style

--- Ukol01-filmy-main/src/components/Role/index.jsx
+++ Ukol01-filmy-main/src/components/Role/index.jsx
@@ -4,12 +4,12 @@ import './style.css';
 
 export const Role = (props) => {   
     return (
-        <>
-        <div className="cast">
+        <div  className="cast">
+          <div>
             <p className="actorName">{props.actor} </p>
             <p className="role"> as {props.role}</p>
+          </div>
         </div>
-        </>
     )
 };
 
